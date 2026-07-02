a001dcbf4569ff6ab40ecbc09dac691bb5284d01
AungNyanHtet18 <aungnyanh81@gmail.com>
2026-07-02 22:35:04 +0630

    [Add] product service and flyway migration

---
+++ e-commerce/product-service/src/main/java/com/dev/anh/product/category/Category.java
@@ -0,0 +1,37 @@
+package com.dev.anh.product.category;
+
+
+import java.util.List;
+
+import com.dev.anh.product.product.Product;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	private String description;
+	
+	@OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+	private List<Product> products;
+	
+	
+	
+	
+}
