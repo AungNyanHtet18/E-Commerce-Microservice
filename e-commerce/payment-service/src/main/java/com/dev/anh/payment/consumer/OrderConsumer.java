01f6f28a03f250f600674cc99151c7659006dfd3
AungNyanHtet18 <aungnyanh81@gmail.com>
2026-07-11 12:45:43 +0630

    [Add] kafka configuration for payment service

---
+++ e-commerce/payment-service/src/main/java/com/dev/anh/payment/consumer/OrderConsumer.java
@@ -0,0 +1,16 @@
+package com.dev.anh.payment.consumer;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class OrderConsumer {
+
+//	@KafkaListener(topics = "order-topic", groupId = "myGroup")
+//	public void consumeOrderMsg(String msg) {
+//		log.info(String.format("Consuming the message from order topic:: %s", msg));
+//	}
+	
+}
